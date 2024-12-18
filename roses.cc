172dbfc927dea9fb37f5b51621fc68953dba96bc
UNKNOWN <rajr86642@gmail.com>
2024-12-18 13:35:24 +0530

    new changes made -- > added standard templates, template codes for both java and cpp, also formatted the rest of the code

--- roses.cc
+++ roses.cc
@@ -1,61 +1,35 @@
 #include<bits/stdc++.h>
 using namespace std;
+
 bool cantake(vector<int> &weights, int d,int weight){
-    int days=1,load=0;
+    int days = 1, load = 0;
     for(int i=0;i<weights.size();i++){
         if(load+weights[i]>weight){
             days++;
             load=weights[i];
-
         }
-        else{
-            load+=weights[i];
-
-        }
+        else load += weights[i];
     }
-    return days<=d;
-
-   
+    return days <= d;
 }
 
-
-
-int leastWeightCapacity(vector<int> &weights, int d)
-{
-    int hi,lo,mid;
-    hi=accumulate(weights.begin(),weights.end(),0);
-    lo=*max_element(weights.begin(),weights.end());
-    while (hi-lo>1)
-    {
-        mid=(hi+lo)/2;
-        if(cantake(weights,d,mid)){
-            hi=mid;
-        }
-        else{
-            lo=mid+1;
-        }
-        
+int leastWeightCapacity(vector<int> &weights, int d) {
+    int hi, lo, mid;
+    hi = accumulate(weights.begin(), weights.end(), 0);
+    lo = *max_element(weights.begin(), weights.end());
+    while (hi-lo>1) {
+        mid = (hi + lo) / 2;
+        if(cantake(weights, d, mid)) hi = mid;
+        else lo = mid + 1;        
     }
-    if(cantake(weights,d,lo)){
-        cout<<lo<<endl;
-    }
-    else{
-        cout<<hi<<endl;
-    }
-
-    
+    if(cantake(weights, d, lo)) return lo;
+    else return hi;
 }
-
-
-
 int main(){
-    int n,d;
-    cin>>n>>d;
+    int n , d;
+    cin >> n >> d;
     vector<int> weights(n);
-    for(int i=0;i<n;i++){
-        cin>>weights[i];
-    }
+    for (int i = 0; i < n; i++) cin >> weights[i];
     leastWeightCapacity(weights, d);
-
-
+    return 0;
 }
